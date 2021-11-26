31f5318aa7f763cf5c88b20d30bb39c57a96bdce
Tom Weaver <atomweavermail@gmail.com>
2021-11-25 18:31:31 -0600

    added modal form to add new books

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 const libraryContainer = document.querySelector('.libraryContainer');
 const addBtn = document.getElementById('add-btn');
+const modal = document.querySelector('.modal');
+const modalAddBookBtn = document.getElementById('add-book-btn');
 
 let myLibrary = [];
 
-function Book(title, author, pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
+class Book {
+    constructor(title, author, pages, hasRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.hasRead = hasRead;
+    }
+}
+
+function clearModal() {
+    document.getElementById('new-title').value = '';
+    document.getElementById('new-author').value = '';
+    document.getElementById('new-pages').value = '';
+    document.getElementById('new-read-status').checked = '';
+    modal.style.display = 'none'
 }
 
 function addBookToLibrary() {
-    const userBook = new Book(prompt('Title: '), prompt('Author: '), prompt('Pages: '));
-    myLibrary.push(userBook);
-    displayBooks();
+    const newTitle = document.getElementById('new-title').value;
+    const newAuthor = document.getElementById('new-author').value;
+    const newPages = document.getElementById('new-pages').value;
+    const newReadStatus = document.getElementById('new-read-status').checked;
+    const newBook = new Book(newTitle, newAuthor, newPages, newReadStatus);
+    myLibrary.push(newBook);
+    clearModal();
+    refreshLibraryDisplay();
 }
 
-function displayBooks() {
+function refreshLibraryDisplay() {
     // loop through library array and display the books
     libraryContainer.innerHTML = '';
     return myLibrary.forEach((element) => {
         const container = document.createElement('div');
         container.classList.add('book');
-        container.innerText = `${element.title}\n${element.author}\n${element.pages}`;
+        container.innerText = `${element.title}\n${element.author}\n${element.pages}\n${element.hasRead}`;
         libraryContainer.appendChild(container);
     });
 }
@@ -30,4 +48,9 @@ function toggleReadStatus(book) {
     book.hasRead === true ? false : true;
 }
 
-addBtn.addEventListener('click', addBookToLibrary);
+// open, close, and submit modal
+addBtn.addEventListener('click', () => modal.style.display = 'block');
+window.addEventListener('click', (e) => {
+    if (e.target === modal) modal.style.display = 'none';
+});
+modalAddBookBtn.addEventListener('click', addBookToLibrary);
